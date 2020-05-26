47ec9c887771e4d91a5ff8575b1cc7575fe834a5
Oleksandr Kolomiichuk <alexkolom27@gmail.com>
2020-05-26 02:01:26 +0200

    Create bottom navigation

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,23 +1,64 @@
+import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 import { Platform } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+
 import { MainScreen } from '../screens/MainScreen'
 import { PostScreen } from '../screens/PostScreen'
+import { BookedScreen } from '../screens/BookedScreen'
 import { THEME } from '../theme'
 
-const PostNavigator = createStackNavigator({
-    Main: MainScreen,
-    Post: {
-        screen: PostScreen
-    }
-}, {
-    initialRouteName: 'Main',
+const navigatorOptions = {
     defaultNavigationOptions: {
         headerStyle: {
             backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff'
         },
         headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR
     }
+}
+
+const PostNavigator = createStackNavigator({
+    Main: MainScreen,
+    Post: PostScreen
+}, navigatorOptions)
+
+const BookedNavigator = createStackNavigator({
+    Booked: BookedScreen,
+    Post: PostScreen
+}, navigatorOptions)
+
+const bottomTabsConfig = {
+    Post: {
+        screen: PostNavigator,
+        navigationOptions: {
+            tabBarLabel: 'All',
+            tabBarIcon: info => <Ionicons name='ios-albums' size={25} color={info.tintColor} />
+        }
+    },
+    Booked: {
+        screen: BookedNavigator,
+        navigationOptions: {
+            tabBarLabel: 'Bookmarked',
+            tabBarIcon: info => <Ionicons name='ios-star' size={25} color={info.tintColor} />
+        }
+    }
+}
+
+const BottomNavigator = Platform.OS === 'android' 
+    ? createMaterialBottomTabNavigator(bottomTabsConfig, {
+        activeTintColor: '#fff',
+        // shifting: true, // with it just active tab has title
+        barStyle: {
+            backgroundColor: THEME.MAIN_COLOR
+        }
+    }) 
+    : createBottomTabNavigator(bottomTabsConfig, {
+    tabBarOptions: {
+        activeTintColor: THEME.MAIN_COLOR
+    }
 })
 
-export const AppNavigation = createAppContainer(PostNavigator)
+export const AppNavigation = createAppContainer(BottomNavigator)
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,25 +1,15 @@
 import React from 'react'
-import { View, StyleSheet, FlatList } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import { DATA } from '../data'
-import { Post } from '../components/Post'
 import { AppHeaderIcon } from '../components/AppHeaderIcon'
+import { PostList } from '../components/PostList'
 
 export const MainScreen = ({navigation}) => {
-
     const openPostHandler = post => {
         navigation.navigate('Post', { postId: post.id, date: post.date, booked: post.booked })
     }
 
-    return (
-        <View style={styles.wrapper}>
-            <FlatList 
-                data={DATA}
-                keyExtractor={post => post.id.toString()}
-                renderItem={({ item }) => <Post post={item} onOpen={openPostHandler} />}
-            />
-        </View>
-    )
+    return <PostList data={DATA} onOpen={openPostHandler} />
 }
 
 MainScreen.navigationOptions = {
@@ -43,9 +33,3 @@ MainScreen.navigationOptions = {
         </HeaderButtons>
     )
 }
-
-const styles = StyleSheet.create({
-    wrapper: {
-        padding: 10
-    }
-})
